00d6aa8e32a14bf5a38e9e63445d96dfea0caf41
MACF\muhammad.ghifari <muhammad.ghifari@mcf.co.id>
2023-06-16 10:00:21 +0700

    catalogue filter

--- server.js
+++ server.js
@@ -103,6 +103,15 @@ app.get("/catalogue", async (req, res) => {
   }
 });
 
+app.post("/catalogue/filter", async (req, res) => {
+  try {
+    const data = await Catalogue.find(req.body);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 app.post("/catalogue", async (req, res) => {
   try {
     var data;
