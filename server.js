e7ccdf6ce249b6a8cd70a5faef4e85e78f2ab725
MACF\muhammad.ghifari <muhammad.ghifari@mcf.co.id>
2023-06-20 17:32:45 +0700

    revoke

--- server.js
+++ server.js
@@ -27,23 +27,23 @@ app.get("/guthib", (req, res) => {
 app.get("/catalogues", async (req, res) => {
   try {
     const data = await Catalogue.find({});
-    const categoryOrder = [
-      "Electronic",
-      "HP",
-      "Home Appliance",
-      "Furniture",
-      "Laptop",
-      "Tablet",
-      "Smart Watch",
-      "Earbuds",
-      "",
-    ];
-    const sortedData = data.sort((a, b) => {
-      const categoryA = categoryOrder.indexOf(a.category);
-      const categoryB = categoryOrder.indexOf(b.category);
-      return categoryA - categoryB;
-    });
-    res.status(200).json(sortedData);
+    // const categoryOrder = [
+    //   "Electronic",
+    //   "HP",
+    //   "Home Appliance",
+    //   "Furniture",
+    //   "Laptop",
+    //   "Tablet",
+    //   "Smart Watch",
+    //   "Earbuds",
+    //   "",
+    // ];
+    // const sortedData = data.sort((a, b) => {
+    //   const categoryA = categoryOrder.indexOf(a.category);
+    //   const categoryB = categoryOrder.indexOf(b.category);
+    //   return categoryA - categoryB;
+    // });
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
