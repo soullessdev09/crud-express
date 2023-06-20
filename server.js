3425376ec6fcfd8fa49b41dc7b761dc5b5d58030
MACF\muhammad.ghifari <muhammad.ghifari@mcf.co.id>
2023-06-20 17:15:47 +0700

    sorted data fix

--- server.js
+++ server.js
@@ -26,14 +26,24 @@ app.get("/guthib", (req, res) => {
 
 app.get("/catalogues", async (req, res) => {
   try {
-    const dataget = await Catalogue.find({});
-    // const categoryOrder = ["Electronic", "HP", "Home Appliance", "Furniture"];
-    // const sortedData = data.sort((a, b) => {
-    //   const categoryA = categoryOrder.indexOf(a.category);
-    //   const categoryB = categoryOrder.indexOf(b.category);
-    //   return categoryA - categoryB;
-    // });
-    res.status(200).json(dataget);
+    const data = await Catalogue.find({});
+    const categoryOrder = [
+      "Electronic",
+      "HP",
+      "Home Appliance",
+      "Furniture",
+      "Laptop",
+      "Tablet",
+      "Smart Watch",
+      "Earbuds",
+      "",
+    ];
+    const sortedData = data.sort((a, b) => {
+      const categoryA = categoryOrder.indexOf(a.category);
+      const categoryB = categoryOrder.indexOf(b.category);
+      return categoryA - categoryB;
+    });
+    res.status(200).json(sortedData);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
