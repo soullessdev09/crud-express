87e10100e98fcdb2b5863bcafe71724c9fe2e6a9
MACF\muhammad.ghifari <muhammad.ghifari@mcf.co.id>
2023-06-30 17:13:19 +0700

    add upload file

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ const Banner = require("./models/bannerModel");
 const Common = require("./models/commonDataModel");
 const Article = require("./models/articleModel");
 const cors = require("cors");
+const formidable = require("formidable");
+const ftp = require("ftp");
 const app = express();
 const port = 8000;
 const mongo_url =
   "mongodb+srv://kreduit:XDMgV9rww96wiKSH@cluster0.adkgvov.mongodb.net/?retryWrites=true&w=majority"; //url kreduit
 // const mongo_url =
 //   "mongodb+srv://nerogama93:2ifA2s7NusdBmc9k@cluster0.xkuwbhh.mongodb.net/Node-API?retryWrites=true&w=majority"; //url nabil
+
+// FTP server credentials
+const ftpConfig = {
+  host: "katalog.kreduit.com", // Replace with the FTP server host
+  port: 21, // Replace with the FTP server port
+  user: "userftp", // Replace with your FTP username
+  password: "F!nt3chit2020!@#$", // Replace with your FTP password
+};
 
 app.use(express.json());
 app.use(cors());
@@ -24,7 +34,162 @@ app.get("/guthib", (req, res) => {
   res.send("Hello Word, you splelled it worng");
 });
 
+// POST IMG
+app.post("/upload", (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.error("Error parsing form:", err);
+      res.status(500).send("Error parsing form");
+      return;
+    }
+
+    const ftpClient = new ftp();
+
+    // Connect to the FTP server
+    ftpClient.connect(ftpConfig);
+
+    // FTP client event handlers
+    ftpClient.on("ready", () => {
+      console.log("FTP client connected");
+
+      // Upload the file to the FTP server
+      const sourcePath = files.image[0].filepath;
+      const destinationPath =
+        "/home/userftp/" + files.image[0].originalFilename;
+
+      ftpClient.put(sourcePath, destinationPath, (uploadErr) => {
+        if (uploadErr) {
+          console.error("Error uploading file:", uploadErr);
+          res.status(500).send("Error uploading file");
+        } else {
+          console.log("File uploaded successfully");
+          res.status(200).json({
+            url_img: `https://katalog.kreduit.com${destinationPath}`,
+          });
+        }
+
+        // Close the FTP connection
+        ftpClient.end();
+      });
+    });
+
+    ftpClient.on("error", (err) => {
+      console.error("FTP client error:", err);
+      res.status(500).send("FTP client error");
+    });
+  });
+});
+
+// Define an Express route to upload an image to the FTP server
+// app.post("/upload", (req, res) => {
+//   const ftpClient = new ftp();
+
+//   // FTP server credentials
+//   const ftpConfig = {
+//     host: "katalog.kreduit.com", // Replace with the FTP server host
+//     port: 21, // Replace with the FTP server port
+//     user: "userftp", // Replace with your FTP username
+//     password: "F!nt3chit2020!@#$", // Replace with your FTP password
+//   };
+
+//   // Path to the local image file
+//   const localFilePath =
+//     "D:/experiment/backendtest/crud-cyclic/opt/download2.jpg";
+
+//   // Destination path on the FTP server
+//   const remoteFilePath = "/home/userftp/download2.jpg";
+
+//   // Connect to the FTP server
+//   ftpClient.connect(ftpConfig);
+
+//   // FTP client event handlers
+//   ftpClient.on("ready", () => {
+//     console.log("FTP client connected");
+
+//     // Read the image file
+//     fs.readFile(localFilePath, (err, data) => {
+//       if (err) {
+//         console.error("Error reading local image file:", err);
+//         res.status(500).send("Error reading local image file");
+//         ftpClient.end(); // Close the FTP connection
+//         return;
+//       }
+
+//       // Upload the image file to the FTP server
+//       ftpClient.put(data, remoteFilePath, (uploadErr) => {
+//         if (uploadErr) {
+//           console.error("Error uploading image file to FTP server:", uploadErr);
+//           res.status(500).send("Error uploading image file to FTP server");
+//         } else {
+//           console.log("Image file uploaded to FTP server");
+//           res.status(200).send("Image file uploaded successfully");
+//         }
+
+//         // Close the FTP connection
+//         ftpClient.end();
+//       });
+//     });
+//   });
+
+//   ftpClient.on("error", (err) => {
+//     console.error("FTP client error:", err);
+//     res.status(500).send("FTP client error");
+//   });
+// });
+
+// POST IMG TEST
+
 // Catalogue API
+
+// app.get("/catalogues", async (req, res) => {
+//   try {
+//     let data;
+//     const page = req.query.page;
+//     const limit = req.query.limit;
+//     const filter = req.query.category;
+//     const search = req.query.search;
+//     if (filter) {
+//       data = await Catalogue.find({ category: filter });
+//     } else {
+//       data = await Catalogue.find({});
+//     }
+//     const categoryOrder = [
+//       "Electronic",
+//       "HP",
+//       "Home Appliance",
+//       "Furniture",
+//       "Laptop",
+//       "Tablet",
+//       "Smart Watch",
+//       "Earbuds",
+//       "",
+//     ];
+//     const sortedData = data.sort((a, b) => {
+//       const categoryA = categoryOrder.indexOf(a.category);
+//       const categoryB = categoryOrder.indexOf(b.category);
+//       return categoryA - categoryB;
+//     });
+//     const searchResult =
+//       search &&
+//       sortedData.filter((item) =>
+//         item.name.toLowerCase().includes(search.toLowerCase())
+//       );
+
+//     const startIndex = (page - 1) * limit;
+//     const endIndex = page * limit;
+//     const pagedData = search
+//       ? searchResult.slice(startIndex, endIndex)
+//       : sortedData.slice(startIndex, endIndex);
+//     const usedData =
+//       page && limit ? pagedData : search ? searchResult : sortedData;
+//     res.status(200).json(usedData);
+//     // res.status(200).json({ data_length: usedData.length, data: usedData });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
 
 app.get("/catalogues", async (req, res) => {
   try {
